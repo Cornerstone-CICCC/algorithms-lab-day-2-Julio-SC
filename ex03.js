a318f2e202c76f5a27e5cb62fe758283e185badb
Julio-SC <jhver2001@gmail.com>
2024-10-01 15:40:50 -0700

    ex03 completed

--- ex03.js
+++ ex03.js
@@ -2,4 +2,21 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+const findMissingNumber = (arr) => {
+    const num = arr.length + 1; 
+    const expectedSum = (num * (num + 1)) / 2;
+    let currentSum = 0;
+    
+    for(let x = 0; x < arr.length; x++ ) {
+        currentSum += arr[x]
+    }
+
+const missingNumber = expectedSum - currentSum;
+return missingNumber
+
+}
+
+
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
+
+
