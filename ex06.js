fdb81915e1a25a2e82f13d7f2e6a74da2da63f24
Julio-SC <jhver2001@gmail.com>
2024-10-01 15:41:40 -0700

    ex06 completed

--- ex06.js
+++ ex06.js
@@ -3,4 +3,21 @@
 // Example: getQueryParams("https://example.com?search=test&page=2")
 // should return { search: "test", page: "2" }.
 
+
+const getQueryParams = (url) => {
+    const queryString = url.split("?")[1];
+    const params = {};
+  
+    if (queryString) {
+      const paramArray = queryString.split("&");
+      for (let i = 0; i < paramArray.length; i++) {
+        const param = paramArray[i].split("=");
+        params[param[0]] = param[1];
+      }
+    }
+  
+    return params;
+  };
+
+
 console.log(getQueryParams("https://example.com?search=test&page=2")); // Expected output: { search: "test", page: "2" }
