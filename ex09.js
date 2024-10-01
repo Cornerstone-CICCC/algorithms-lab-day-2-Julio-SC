569d24e64b45c2d9304cd37bca46166b1bb2f379
Julio-SC <jhver2001@gmail.com>
2024-10-01 15:42:13 -0700

    ex09 completed

--- ex09.js
+++ ex09.js
@@ -3,4 +3,9 @@
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
 
+function sortByProperty(arr, property) {
+    return arr.sort((a, b) => (a[property] > b[property] ? 1 : -1));
+  }
+
+
 console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
